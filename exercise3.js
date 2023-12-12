a5bd40263f4f082688a760309674b35c4a72d0bb
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-12-12 17:18:36 +0100

    exercise ok

---
+++ exercise3.js
@@ -0,0 +1,8 @@
+INSERT INTO books
+VALUES(1, 'A Journey to the Center', 'Jules Verne', 'Adventure', 1864, 1234567890123, 12.99, 4.5, 10, 'Verne Publishing', 350);
+INSERT INTO books
+VALUES(2, 'War and Peace', 'Leo Tolstoy', 'Historical', 1869, 1234567890124, 14.99, 4.8, 5, 'Tolstoy Prints', 1200);
+INSERT INTO books 
+VALUES (3, 'Whispers of the Wind', 'Amelia Blackburn', 'Romance', 1982, 1234567890125, 9.99, 4.2, 20, 'Blackburn House', 275);
+INSERT INTO books 
+VALUES (4, 'The Galactic Odyssey', 'Orion Starfield', 'Science Fiction', 2005, 1234567890126, 19.99, 4.9, 15, 'Nebula Press', 450);
