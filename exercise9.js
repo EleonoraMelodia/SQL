04d860c1d7d93b0ba0196ee46ef56bdd9e862a3a
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-12-18 17:20:48 +0100

    fixed

--- exercise9.js
+++ exercise9.js
@@ -1,15 +1,26 @@
-SELECT * FROM tested WHERE survived = 1 AND sex = 'female' AND age > 30;
-//
-SELECT item FROM tested WHERE survived = 1 AND age = (SELECT MAX(age) FROM tested);
-//
-SELECT * FROM tested WHERE embarked = 'C' AND ticket < 50 AND ticket > 20;
-//
-SELECT COUNT(*)  AS "Number of survivors"
+SELECT *
 FROM tested
-WHERE survived = 1;
+WHERE survived = 1 AND sex = 'female' AND age > 30;
+
+//
+SELECT item
+FROM tested
+WHERE survived = 1 AND age = (SELECT MAX(age) FROM tested WHERE survived = 1);
+
 //
 SELECT *
 FROM tested
-WHERE embarked = 'C' AND ticket > 75;
+WHERE embarked = 'C' AND fare < 50 AND fare > 20;
+
+//
+SELECT COUNT(*) AS "Number of survivors"
+FROM tested
+WHERE survived = 1;
+
+//
+SELECT *
+FROM tested
+WHERE embarked = 'C' AND fare > 75;
+
 
 
